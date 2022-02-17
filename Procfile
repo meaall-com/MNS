19376497f27d9b23a00bd3d22831c879259316d6
meaall-com <67676419+meaall-com@users.noreply.github.com>
2022-02-17 21:04:35 +0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python3 pyro.py
---
+++ pyro.py
@@ -0,0 +1,55 @@
+from posixpath import split
+from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+app = Client("gramsession", 14897586, "49db49b537577139a4b337f2764b70a3")
+toSender = "meaallhtec"
+
+@app.on_message()
+def my_function(client, message):
+    if message.chat.id != -1001159025906:
+        if message.text:
+            textUpdate = message.text
+            splitText = textUpdate.split("||")
+        
+            if '.join' == splitText[0]:
+                chatJuser = app.get_chat(splitText[1])['username']
+                print(app.join_chat(chatJuser))
+            elif '.del' == splitText[0]:
+                chatJuser = app.get_chat(splitText[1])['id']
+                app.leave_chat(chatJuser, delete=True)
+
+        if message.chat.type == "channel" and message.chat.username != toSender:
+            print( message )
+            if message.photo:
+                photoFileId = message.photo.file_id
+                captionPhoto = message.caption
+                IdSenderPhoto = message.chat.id
+                app.send_photo(toSender, photoFileId, caption=f"\(idsender){IdSenderPhoto}\(idSender): {captionPhoto}")
+            if message.video:
+                videoFileId = message.video.file_id
+                captionVideo = message.caption
+                IdSenderVideo = message.chat.id
+                app.send_video(toSender, videoFileId, caption=f"\(idsender){IdSenderVideo}\(idSender): {captionVideo}")
+            if message.document:
+                documentFileId = message.document.file_id
+                captionDocument = message.caption
+                IdSenderDocument = message.chat.id
+                app.send_document(toSender, documentFileId, caption=f"\(idsender){IdSenderDocument}\(idSender): {captionDocument}")
+            if message.audio:
+                audioFileId = message.audio.file_id
+                captionAudio = message.caption
+                IdSenderAudio = message.chat.id
+                app.send_audio(toSender, audioFileId, caption=f"\(idsender){IdSenderAudio}\(idSender): {captionAudio}")
+            if message.text:
+                textIsset = message.text
+                IdSenderText = message.chat.id
+                app.send_message(toSender, f"\(idsender){IdSenderText}\(idSender): {textIsset}")
+
+            
+        
+        
+my_handler = MessageHandler(my_function)
+
+app.add_handler(my_handler)
+
+app.run()
